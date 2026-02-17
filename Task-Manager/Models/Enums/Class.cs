fd2d78fe2a235b7e1a4c5f0d7197373cd4f6b06a
VitorHugo-br <vitor.comercialcontato@gmail.com>
2026-02-17 18:42:00 -0300

    Add task model

---
+++ Task-Manager/Models/Enums/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_Manager.Models.Enums
+{
+    public enum Status
+    {
+        Pending,
+        InProgress,
+        Completed,
+        Delivered,
+    }
+}
---
+++ Task-Manager/Models/MyTask.cs
@@ -0,0 +1,18 @@
+﻿using Task_Manager.Models.Enums;
+
+namespace Task_Manager.Models
+{
+    public class MyTask
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public Status Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public required string RequestedBy { get; set; }
+        public string? SignedTo { get; set; }
+
+    }
+}
